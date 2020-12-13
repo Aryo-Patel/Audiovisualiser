e98a2318f0d6cf478c073773fa9b9b79c7b23333
AryoP <aryopatel@gmail.com>
2020-12-12 18:47:11 -0600

    started working on visualization of the audio

--- client/app.js
+++ client/app.js
@@ -4,62 +4,56 @@ let link = document.getElementById('link');
 //button
 let button = document.getElementById('submit');
 
-//iframe
-//let iframe = document.getElementById('iframe');
+let visualiser = document.getElementById('display');
+const canvasContext = visualiser.getContext('2d');
+let loading = true;
 
-//set iframe opacity to zero if there is no source link provided
-// if(iframe.src === ""){
-//     iframe.style.opacity = 0;
-// }
+canvasContext.beginPath();
+const CIRCLE_RADIUS = 30;
+console.log(visualiser.width)
+console.log(visualiser.height);
+canvasContext.arc(visualiser.width/2, visualiser.height/2, CIRCLE_RADIUS, 0, Math.PI*2);
+canvasContext.stroke();
+
+let pastVal = '';
 
 //on button press, transfer URL from input to the iframe if the input is not empty
 button.addEventListener('click', e =>{
-    if(link.value !== ""){
+    if(link.value !== "" && link.value !== pastVal){
         
         let url = urlParser(link.value);
-        console.log('about to send xml request')
+        pastVal = link.value;
+
+        //create the xml response that gets sent to the server grabs the audio data
         const request = new XMLHttpRequest();
         request.open('POST', `/url/${url}`, true);
         request.responseType = 'arraybuffer';
+        request.onloadstart = function(event){
+            makeSpikes();
+        }
         request.onload = function(){
             let data = request.response;
-            console.log(typeof(data));
+            //audio data passed through this function which applies the web audio api
             parseData(data);
         };
-        request.send();
-        // $.post(`/url/${url}`).done(data => {
-        //     let enc = new TextEncoder();
-        //     data = enc.encode(data);
-        //     //data = new Buffer.from(data, 'utf-8');
-        //     console.log(typeof(data));
-        //     parseData(data);
-        // });
 
-        //iframe.src = url;
-        //iframe.style.opacity =1;
+        request.send();
     }
 });
+function makeSpikes(){
+    console.log('this function is executing');
+    // while(loading){
+    //     let x = visualiser.width/2 - CIRCLE_RADIUS;
+    //     let y = visualiser.height/2
 
-// function loadSound() {
-//     var request = new XMLHttpRequest();
-//     request.open("GET", "http://localhost:8000/stream/nl6OW07A5q4", true); 
-//     request.responseType = "arraybuffer"; 
-  
-//     request.onload = function() {
-//         var Data = request.response;
-//         process(Data);
-//     };
-  
-//     request.send();
-//   }
-  
-//   function process(Data) {
-//     source = context.createBufferSource(); // Create Sound Source
-//     context.decodeAudioData(Data, function(buffer){
-//       source.buffer = buffer;
-//       source.connect(context.destination); 
-//       source.start(context.currentTime);
-//   }) 
+    //     setInterval(() => {
+    //         console.log('ran');
+    //         canvasContext.fillRect(x, y, 10, 10)
+    //         x += 5;
+    //         y = Math.sqrt(CIRCLE_RADIUS**2 - (x - visualiser.width/2)**2);
+    //     }, 100);
+    // }
+}
 
 //turns whatever the user passed in into an embed YT link otherwise it alerts the user of an invalid input
 function urlParser(url){
@@ -78,23 +72,44 @@ function urlParser(url){
         alert('Please submit from youtube')
     }
     return returnString
-    //return `https://youtube.com/embed/${returnString}`
 }
 
+const audioContext = new  AudioContext();
+const analyserNode = new AnalyserNode(audioContext, {fftSize: 64, smoothingTimeConstant: 0.8});
 
 function parseData(data){
-    console.log(typeof(data));
     //audio context setup
-    const audioContext = new  AudioContext();
     let source =audioContext.createBufferSource();
     audioContext.decodeAudioData(data, function(buffer){
         source.buffer = buffer;
-        source.connect(audioContext.destination);
+        source
+            .connect(analyserNode)
+            .connect(audioContext.destination);
         source.start();
+        getAudioData();
     });
-    // let source = audioContext.createBufferSource()
-    // source.buffer = data;
+}
+function getAudioData(){
+    requestAnimationFrame(getAudioData);
 
-    // source.connect(audioContext.destination);
-    // source.start();
+    const bufferLength = analyserNode.frequencyBinCount;
+
+    const dataArray = new Uint8Array(bufferLength);
+
+    analyserNode.getByteFrequencyData(dataArray);
+
+    const width = visualiser.width;
+    const height =visualiser.height;
+
+
+    const barWidth =width/bufferLength;
+
+    canvasContext.clearRect(0,0, width, height);
+    for(let i = 0; i < dataArray.length; i++){
+        let y = dataArray[i]/255 * height/2;
+        const x = barWidth*i;
+
+        canvasContext.fillStyle = `hsl(${i*360/dataArray.length}, 100%, 50%)`;
+        canvasContext.fillRect(x, height-y, barWidth, y);
+    }
 }
